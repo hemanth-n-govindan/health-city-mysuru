3bf385686aaa4b8393eb79b6e6f3d72c949ad862
Hemanth N Govindan <Hemanth.NG612@gmail.com>
2025-07-04 22:45:21 +0530

    logo change

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,9 +15,9 @@
 
     // === logo change
     if (ud_header.classList.contains("sticky")) {
-      logo.src = "assets/images/logo/logo-2.png";
+      logo.src = "assets/images/logo/new-black.png";
     } else {
-      logo.src = "assets/images/logo/logo.png";
+      logo.src = "assets/images/logo/new.png";
     }
 
     // show or hide the back-top-top button
